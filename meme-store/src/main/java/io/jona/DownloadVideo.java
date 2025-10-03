0e9a717bda3936f0555ae81bfda21cd15778eb73
barri <barrientosjonah@gmail.com>
2025-10-03 10:40:54 -0300

    DownloadVideo is already working, and use a simple logging API, and I changed the version of the SLF4J API in simple-http-server to a newer

--- meme-store/src/main/java/io/jona/DownloadVideo.java
+++ meme-store/src/main/java/io/jona/DownloadVideo.java
@@ -1,28 +1,31 @@
 package io.jona;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.FileInputStream;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class DownloadVideo {
+    private static final Logger logger = LoggerFactory.getLogger(DownloadVideo.class);
     private static final Properties props = new Properties();
     public static void main(String[] args) {
+        //https://www.tiktok.com/@gertu0k/video/7258798530066713862
+        //https://www.instagram.com/reel/DOWR1MPDaE4/?igsh=MTR6N3duemJmd3R0eA%3D%3D
+        //https://www.youtube.com/shorts/Msn1xgzBwTg
         //https://www.facebook.com/reel/718363154596932
-        runDownloader("https://www.youtube.com/shorts/Msn1xgzBwTg");
+        runDownloader("https://www.facebook.com/reel/718363154596932");
     }
 
     public static void runDownloader(String url) {
         Platforms platform = Platforms.whatPlatformIs(url);
         String[] command = new String[4];
-
-
-        System.out.println("user.dir: "+ System.getProperty("user.dir"));
-
+        logger.info("user.dir: "+ System.getProperty("user.dir"));
         try (FileInputStream fis = new FileInputStream("./app.properties")) {
-
+            props.load(fis);
             String ytDl = props.getProperty("ytDl.win");
             String youtubeDl = props.getProperty("youtubeDl.win");
             String destinyFolder = props.getProperty("videOutputPath");
-            props.load(fis);
             switch (platform) {
                 case FACEBOOK:
                     buildCommand(command, ytDl, destinyFolder, url);
@@ -37,22 +40,24 @@ public class DownloadVideo {
                     buildCommand(command, ytDl, destinyFolder, url);
                     break;
                 default:
-                    System.out.println("Invalid url");
+                    logger.debug("Invalid url");
                     return;
             }
             Process process = Runtime.getRuntime().exec(command);
             process.waitFor();
-            System.out.println("Download finished");
+            logger.debug("Download finished");
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Download failed", e);
         }
     }
 
     public static String[] buildCommand(String[] command, String downloader, String destinyFolder, String url) {
+        logger.trace("Entering buildCommand() with params command={}, downloader={}, destinyFolder={}, url={}", command, downloader, destinyFolder, url);
         command[0] = downloader;
         command[1] = "-o";
         command[2] = destinyFolder + "/%(title)s.%(ext)s";
         command[3] = url;
+        logger.trace("Returning full command of buildCommand() method: {}", Arrays.toString(command));
         return command;
     }
 }
